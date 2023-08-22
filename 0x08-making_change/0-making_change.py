1f9549722a9aa75ca1a437768281673fa147fac3
Eceeomar <isiomaenuonye@gmail.com>
2023-08-22 15:05:21 +0100

    fixed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
 """
-Main file for testing
+Changes come from within
 """
 
-makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 2, 25], 37))
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values determine,
+    the fewest number of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
 
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
+
+
+if __name__ == "__main__":
+    coins = [1, 2, 25]
+    total = 37
+    result = makeChange(coins, total)
+    print(result)
